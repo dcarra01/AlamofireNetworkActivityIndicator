6e6bf2e1e8ba45ed02959174fbf69fa1989a25e3
Diego Carrera <dcarrera@binima.com>
2020-02-25 01:29:15 +0100

    updated

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(name: "AlamofireNetworkActivityIndicator",
                       platforms: [.iOS(.v10)],
                       products: [.library(name: "AlamofireNetworkActivityIndicator", targets: ["AlamofireNetworkActivityIndicator"])],
-                      dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.2")],
+                      dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0")],
                       targets: [.target(name: "AlamofireNetworkActivityIndicator",
                                         dependencies: ["Alamofire"],
                                         path: "Source")])
